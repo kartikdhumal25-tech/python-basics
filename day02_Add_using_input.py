3a320cedefa47e01e17258e308745c4cb7f6a029
Kartik Dhumal <kartikdhumal5555@gmail.com>
2026-01-06 10:47:26 +0530

    Update day02_Add_using_input.py
    Day 02: addition using input practice

--- day02_Add_using_input.py
+++ day02_Add_using_input.py
@@ -1,5 +1,5 @@
-a = int(input("Enter a first number:"))
-b = int(input("Enter a second number:"))
+a = int(input("Enter a first number:")) #input()takes value from user (keyboard)
+b = int(input("Enter a second number:")) #int()converts input into number
 
 sum = a+b
 
